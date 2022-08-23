a10ac2b866cdbde7c4731a9c65e2eaf837cb1eb5
prajvalsinghOD <singhprajval91@gmail.com>
2022-08-23 16:51:26 +0530

    basic setup

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,80 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private Integer age;
+    private LocalDate Dob;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, Integer age, LocalDate dob, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        Dob = dob;
+        this.email = email;
+    }
+
+    public Student(String name, Integer age, LocalDate dob, String email) {
+        this.name = name;
+        this.age = age;
+        Dob = dob;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", Dob=" + Dob +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setDob(LocalDate dob) {
+        Dob = dob;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public LocalDate getDob() {
+        return Dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
